092147da839f21f87689f3a0e078afcf0febbee4
Jordan Irwin <antumdeluge@gmail.com>
2021-05-20 18:32:25 -0700

    Cleanup

--- init.lua
+++ init.lua
@@ -2,18 +2,18 @@ dofile(minetest.get_modpath("whinny").."/api.lua")
 dofile(minetest.get_modpath("whinny").."/horse.lua")
 
 minetest.register_craftitem("whinny:meat", {
-    description = "Cooked Meat",
-    inventory_image = "whinny_meat.png",
-    on_use = minetest.item_eat(4),
+	description = "Cooked Meat",
+	inventory_image = "whinny_meat.png",
+	on_use = minetest.item_eat(4),
 })
 
 minetest.register_craftitem("whinny:meat_raw", {
-    description = "Raw Meat",
-    inventory_image = "whinny_meat_raw.png",
+	description = "Raw Meat",
+	inventory_image = "whinny_meat_raw.png",
 })
 
 minetest.register_craft({
-    type = "cooking",
-    output = "whinny:meat",
-    recipe = "whinny:meat_raw",
+	type = "cooking",
+	output = "whinny:meat",
+	recipe = "whinny:meat_raw",
 })
